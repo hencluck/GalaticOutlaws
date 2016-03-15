456b74596fc20fad3ad219c645c30352f56dd603
Henry Barton <barton.henry@gmail.com>
2016-03-15 10:31:10 +0000

    Added Game Repository
    Hopefully this is all working and set to go, let me know if it is not...
    
    Henry

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+    [System.Serializable]
+    public class EnemyStats
+    {
+
+        public int maxHealth = 100;
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
+
+        public int damage = 10;
+
+        public void init()
+        {
+            curHealth = maxHealth;
+
+        }
+
+    }
+    public Transform deathParticles;
+    public EnemyStats Stats = new EnemyStats();
+
+ 
+
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
+    {
+
+        Stats.init();
+
+        if(statusIndicator != null)
+        {
+            statusIndicator.SetHealth(Stats.curHealth, Stats.maxHealth);
+        }
+
+        if(deathParticles == null)
+        {
+            Debug.LogError("no death particles found");
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D _colInfo)
+    {
+        Debug.LogError("collison made");
+        Player _player = _colInfo.collider.GetComponent<Player>();
+        if (_player != null)
+        {
+            Debug.LogError("did 10 damage");
+            _player.DamagePlayer(Stats.damage);
+            DamageEnemy(999999);
+
+        }
+    }
+
+    public void DamageEnemy(int damage)
+    {
+
+        Stats.curHealth -= damage;
+
+        if (Stats.curHealth <= 0)
+        {
+            GameMaster.KillEnemy(this);
+        }
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(Stats.curHealth, Stats.maxHealth);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster gm;
+
+    
+
+    void Start()
+	{
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster> ();
+		}
+	}
+
+    public Transform spawnPoint;
+    public Transform playerPrefab;
+    public Transform spawnPrefab;
+    public int spawnDelay = 2;
+	public Transform mainCamera;
+	public Transform CameraRespawn;
+	public GameObject Player;
+
+  
+
+
+
+
+    public IEnumerator RespawnPlayer()
+    {
+		
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        Destroy(clone, 3f);
+		mainCamera.position = CameraRespawn.position;
+		//Player.GetComponent<Player> ();
+		//Player.Stats.init ();
+	}
+		
+
+
+
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+
+    public static void KillEnemy(Enemy enemy)
+    {
+        gm._KillEnemy(enemy);
+
+    }
+
+    public void _KillEnemy(Enemy _enemy)
+    {
+        Instantiate(_enemy.deathParticles, _enemy.transform.position, Quaternion.identity);
+        Destroy(_enemy.gameObject);
+    }
+
+}
